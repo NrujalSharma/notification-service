6a3424c55b7e2548a2af349947db7af51f1ca189
NrujalSharma <snrujal@gmail.com>
2021-05-16 15:42:07 +0530

    Use DB creds from .env file

--- src/database/createConnectionPool.ts
+++ src/database/createConnectionPool.ts
@@ -5,10 +5,10 @@ let connectionPool: mysql.Pool;
 export async function getConnectionPool() {
     if (!connectionPool) {
         connectionPool = await mysql.createPool({
-            host: 'localhost',
-            user: 'root',
-            password: 'root',
-            database: 'notification_service',
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
             waitForConnections: true,
             connectionLimit: 10,
             queueLimit: 0
